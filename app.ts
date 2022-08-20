bd1ae3fb06dfd59b11764cecff40d36228cd86a1
Octo8080 <okutannderuta0529@gmail.com>
2022-08-20 13:13:31 +0900

    commit

--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
 import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+import { v4 } from "https://deno.land/std@0.152.0/uuid/mod.ts";
+
+let s = ""
+let c = 0
 
 function a(){
     setInterval(()=>{
-        console.log(new Date)
+        c++
+        console.log(`${s}:${c}`)
     },1000)
 }
 
 serve((req) => {
     if(req.url.search("/favicon.ico")<0){
       a()
+      s = crypto.randomUUID();
     }
     return new Response("Hello World\n");
 }, {port:8080});
